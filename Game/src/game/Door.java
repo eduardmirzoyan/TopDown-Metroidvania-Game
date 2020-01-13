384032e7fad1a19b5f3dac38a3a27e4970daeb87
Eduard Mirzoyan <eduard.mirzoyan123@gmail.com>
2020-01-13 12:35:10 -0800

    Added comments to Door and Key class

--- Game/src/game/Door.java
+++ Game/src/game/Door.java
@@ -16,20 +16,37 @@ public class Door {
 		color = col;
 		sprite = img;
 	}
-	
-	
+	/**
+	 * 
+	 * @param i Sets door sprite to input
+	 */
 	public void setSprite(PImage i) {
 		sprite = i;
 	}
 	
+	/**
+	 * 
+	 * @param i Sets door color to input string
+	 */
 	public void setColor(String i) {
 		color = i;
 	}
 	
+	/**
+	 * 
+	 * @return Returns the color of the door
+	 */
 	public String getColor() {
 		return color;
 	}
 	
+	/**
+	 * 
+	 * @param p The PApplet used to draw the inventory elements
+	 * @param x The topleft x cord of the inventory module
+	 * @param y The topleft y cord of the inventory module
+	 * @param size
+	 */
 	public void draw(PApplet p, int x, int y, int size) {
 		p.pushMatrix();
 		
@@ -38,6 +55,11 @@ public class Door {
 		p.popMatrix();
 	}
 	
+	/**
+	 * Checks to see if the player's inventory has a key that can open this door
+	 * @param inv The inventory used to check if a key exists
+	 * @return True or false depending if a key exists that matches this door's color
+	 */
 	public boolean unlock(Inventory inv) {
 		if(inv.removeKey(color)) {
 			return true;
--- Game/src/game/Key.java
+++ Game/src/game/Key.java
@@ -17,18 +17,37 @@ public class Key {
 		sprite = img;
 	}
 	
+	/**
+	 * 
+	 * @param i The image to set this key's sprite
+	 */
 	public void setSprite(PImage i) {
 		sprite = i;
 	}
 	
+	/**
+	 * 
+	 * @param i The color to set this key's color
+	 */
 	public void setColor(String i) {
 		color = i;
 	}
 	
+	/**
+	 * 
+	 * @return The color of this key
+	 */
 	public String getColor() {
 		return color;
 	}
 	
+	/**
+	 * 
+	 * @param p The PApplet to draw this key on
+	 * @param x The topleft x cord of the key
+	 * @param y The topleft y cord of the key
+	 * @param size The height and width of the key
+	 */
 	public void draw(PApplet p, int x, int y, int size) {
 		p.pushMatrix();
 		
